01a282c5382e2661f15dfb04e5b8d7d813581314
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-09-25 23:07:36 +0000

    Acessando api

--- lib/edools_desafio.rb
+++ lib/edools_desafio.rb
@@ -1,5 +1,8 @@
 require "edools_desafio/version"
+require "edools_desafio/configurattion"
 
 module EdoolsDesafio
-  # Your code goes here...
+  def self.all
+		Unirest.get(Configurattion::URL_BASE)#.body.map { |learn| Learn.new(learn) }
+	end
 end
---
+++ lib/edools_desafio/configurattion.rb
@@ -0,0 +1,3 @@
+module Configurattion
+	URL_BASE = "https://sua-escola.myedools.com/api"
+end
